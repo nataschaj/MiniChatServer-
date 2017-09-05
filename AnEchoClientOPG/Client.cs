e5d835b539a0f5157d4f8989a96a5e8d2716ceac
natsachaj <natascha_jakobsen@hotmail.com>
2017-09-05 14:09:19 +0200

    Client Class tilføjet

---
+++ AnEchoClientOPG/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnEchoClientOPG
+{
+    class Client
+    {
+        public void Start()
+        {
+
+        }
+    }
+}
